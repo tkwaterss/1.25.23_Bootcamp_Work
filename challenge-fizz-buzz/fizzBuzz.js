7329523b366d7fbffb5b3bec09e28bb275e1c277
Tobin Waters <tkwaterss@gmail.com>
2023-01-25 08:20:09 -0700

    Added comments to my code

--- challenge-fizz-buzz/fizzBuzz.js
+++ challenge-fizz-buzz/fizzBuzz.js
@@ -1,11 +1,18 @@
 // Write your solution below this line:
+
+//Starting w/ for loop to cycle from 1 to 50
 for (i = 1; i <= 50; i++) {
+    //Using a series of if else statements to check for conditions
+    //first if i is divisible by both 3 and 5
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("fizzbuzz");
+        //then if divisible by 3
     } else if (i % 3 === 0) {
         console.log("fizz");
+        //then if divisible by 5
     } else if (i % 5 === 0) {
         console.log("buzz");
+        //if not divisible then print the number
     } else {
         console.log(i);
     }
